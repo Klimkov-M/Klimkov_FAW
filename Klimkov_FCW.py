31d034486f1239e655c31d68724a352e189740b2
Klimkov-M <M.Y.Klimkov@gmail.com>
2024-11-06 22:21:12 +0300

    Добавлены комментарии к каждой строке кода.

--- Klimkov_FCW.py
+++ Klimkov_FCW.py
@@ -1,12 +1,13 @@
-from tkinter import *
+from tkinter import *  # Библиотека для создания графического интерфейса программы с виджетами.
 
 
-w = Tk()
-w.title('Конвектор криптовалют.')
-display_width = w.winfo_screenwidth()
-display_height = w.winfo_screenheight()
-w_width = 600
-w_height = 400
-w.geometry(f'{w_width}x{w_height}+{display_width//2 - w_width//2}+{display_height//2 - w_height//2}')
+w = Tk()  # Создаём окно интерфейса программы.
+w.title('Конвектор криптовалют.')  # Даём окну название.
+display_width = w.winfo_screenwidth()  # Запрашиваем размер монитора по оси икс в пикселях.
+display_height = w.winfo_screenheight()  # Запрашиваем размер монитора по оси игрек в пикселях.
+w_width = 600  # Переменная, в которой хранится заданная ширнина окна в пикселях.
+w_height = 400  # Переменная, в которой хранится заданная высота окна в пикселях.
+w.geometry(f'{w_width}x{w_height}+{display_width//2 - w_width//2}+{display_height//2 - w_height//2}')  # Устанавливаем
+# размер окна и размещаем окно программы по центру монитора.
 
-w.mainloop()
+w.mainloop()  # Запускаем основной цикла событий GUI.
